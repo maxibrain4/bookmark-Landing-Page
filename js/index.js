96ac99d031f297a4d9586ebe399bbd01a1eba025
Fakunle Israel <maxibrain4@gmail.com>
2023-06-10 21:51:19 +0100

    finished the project

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
-
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 //Tabs Menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
 
@@ -25,4 +27,19 @@ function onTabClick(e) {
     .getElementById("panels")
     .getElementsByClassName(classString)[0]
     .classList.remove("hidden");
+}
+
+// button
+btn.addEventListener("click", navToggle);
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
 }
